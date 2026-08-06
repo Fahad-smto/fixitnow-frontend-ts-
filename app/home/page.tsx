31194bcb95624daefa1d8f509eecafb703f5cfed
Fahad-smto <simanto.smto.prog@gmail.com>
2026-08-06 17:57:12 +0600

    footer section is add

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,6 +2,7 @@
  import ServicePromoBanner from '@/components/ui/banner';
 import PopularServices from '@/components/ui/popularServices';
 import TechnicianTrust from '@/components/ui/TechnicianTrust';
+import Footer from '@/components/ui/footer';
 
  const home = () => {
    return (
@@ -9,6 +10,7 @@ import TechnicianTrust from '@/components/ui/TechnicianTrust';
    <ServicePromoBanner></ServicePromoBanner>
    <PopularServices></PopularServices>
    <TechnicianTrust></TechnicianTrust>
+   <Footer></Footer>
      </div>
    );
  };
