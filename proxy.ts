670771360caa0720024f53d513f7093572c6b32f
Fahad-smto <simanto.smto.prog@gmail.com>
2026-09-03 23:30:44 +0600

    code

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,3 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
 
 // Routes anyone can visit without logging in
 const PUBLIC_ROUTES = ['/','/home', '/services', '/technicians', '/payment/success', '/payment/cancel'];
